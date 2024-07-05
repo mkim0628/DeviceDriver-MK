a55a4af126d266dcb5d447ab52a4c2bf1e140c96
Myeong Woo Kim <mkim0628@gmail.com>
2024-07-05 09:03:07 +0900

    first commit

---
+++ DeviceDriver/DeviceDriver.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "FlashMemoryDevice.h"
+
+class DeviceDriver
+{
+public:
+    DeviceDriver(FlashMemoryDevice* hardware);
+    int read(long address);
+    void write(long address, int data);
+
+   
+protected:
+    FlashMemoryDevice* m_hardware;
+};
